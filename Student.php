b958257bbb8474e787b385df0f21b4997000fd79
Cooluke75 <luke.lee7575@gmail.com>
2018-01-11 15:09:36 -0800

    Added comments

--- Student.php
+++ Student.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Student.php
  * Student class that holds information on the students such as first name,
